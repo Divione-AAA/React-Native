c818c45cba99ee47109161ca17bf9664c248df54
Hector David <hectordavid55555211@gmail.com>
2026-01-08 14:29:37 -0500

    commit

--- RestaurantesApp/app/navigation.tsx
+++ RestaurantesApp/app/navigation.tsx
@@ -1,8 +1,10 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'react-native';
 
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
+      
       <Stack.Screen name="HomeScreen"/>
       <Stack.Screen name="restaurantScreen" />
     </Stack>
